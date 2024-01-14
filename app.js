0ba182d92bbea1424e66e4ef890cb97273a998be
Liviu <liviu.panainte@gmail.com>
2024-01-14 22:22:51 +0000

    Update socket server

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ const Redis = require("redis");
 const wss = new WebSocket.Server({port: 8088});
 var redisClient = Redis.createClient();
 
+console.log("started server");
+
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         redisClient.lpush("commands", message);
     });
+
+    ws.on("error", (err) => {
+        console.log(err.stack);
+    });
 });
+
+
 
 wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
